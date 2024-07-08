d039e4bcc75be225356eebe4ccc3fded38fa611b
Herik Santos <heriksantos04@gmail.com>
2024-07-08 09:41:49 -0300

    feat: create CreateUserUseCase

---
+++ src/useCases/User/CreateUserController.ts
@@ -0,0 +1,21 @@
+import { type Request, type Response } from "express";
+
+import { type IController } from "../protocols/IController";
+import { type ICreateUserUseCase } from "./ICreateUserUseCase";
+
+class CreateUserController implements IController {
+    private readonly createUserUseCase: ICreateUserUseCase;
+    constructor(createUserUseCase: ICreateUserUseCase) {
+        this.createUserUseCase = createUserUseCase;
+    }
+
+    async handle(request: Request, response: Response): Promise<Response> {
+        const data = request.body;
+
+        const user = await this.createUserUseCase.execute(data);
+
+        return response.status(201).json(user);
+    }
+}
+
+export { CreateUserController };
