372bd403b637af6f4135e5846616a1548a45165b
Herik Santos <heriksantos04@gmail.com>
2024-07-09 11:30:55 -0300

    feat: creation error custom BadRequest

--- src/errors/errors.ts
+++ src/errors/errors.ts
@@ -8,4 +8,14 @@ class MissingParams extends Error {
     }
 }
 
-export { MissingParams };
+class BadRequest extends Error {
+    public statusCode: number;
+
+    constructor(message: string, statusCode: number) {
+        super(message);
+        this.statusCode = statusCode;
+        Object.setPrototypeOf(this, BadRequest.prototype);
+    }
+}
+
+export { MissingParams, BadRequest };
