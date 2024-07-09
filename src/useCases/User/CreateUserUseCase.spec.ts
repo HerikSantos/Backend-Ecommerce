c260b1c10dda566b4df24118a826f767c4b8c482
Herik Santos <heriksantos04@gmail.com>
2024-07-09 11:19:45 -0300

    test: implementation userrepository

--- src/useCases/User/CreateUserUseCase.spec.ts
+++ src/useCases/User/CreateUserUseCase.spec.ts
@@ -1,20 +1,44 @@
+import { type IDbUser } from "../../entities/interfaces";
+import { type IUserRepository } from "../../repository/Prisma/IUserRepository";
 import { type IEmailValidator } from "../protocols/IEmailValidator";
 import { type IEncrypterHash } from "../protocols/IEncrypterHash";
 import { CreateUserUseCase } from "./CreateUserUseCase";
+import { type IUser } from "./interfaces";
 
 interface ITypesSut {
     sut: CreateUserUseCase;
     emailValidatorStub: IEmailValidator;
     encrypterHash: IEncrypterHash;
+    userRepositoryStub: IUserRepository;
 }
+
+const repository: IDbUser[] = [];
+
+function makeUserRepository(): IUserRepository {
+    class UserRepositoryStub implements IUserRepository {
+        async add(user: IUser): Promise<IDbUser> {
+            const newUser: IDbUser = { ...user, id: "valid_id" };
+            repository.push(newUser);
+            return newUser;
+        }
+
+        async findById(id: string): Promise<IDbUser> {
+            const user = repository.find((user) => user.id === id);
+            if (!user) throw Error();
+            return user;
+        }
+    }
+    return new UserRepositoryStub();
+}
+
 function makeEncrypter(): IEncrypterHash {
-    class EncrypterHash implements IEncrypterHash {
+    class EncrypterHashStub implements IEncrypterHash {
         hash(): string {
             return "hashed_pas";
         }
     }
 
-    return new EncrypterHash();
+    return new EncrypterHashStub();
 }
 function makeEmailValidatorStub(): IEmailValidator {
     class EmailValidatorStub implements IEmailValidator {
@@ -27,8 +51,14 @@ function makeEmailValidatorStub(): IEmailValidator {
 function makeSut(): ITypesSut {
     const emailValidatorStub = makeEmailValidatorStub();
     const encrypterHash = makeEncrypter();
-    const sut = new CreateUserUseCase(emailValidatorStub, encrypterHash);
-    return { sut, emailValidatorStub, encrypterHash };
+    const userRepositoryStub = makeUserRepository();
+    const sut = new CreateUserUseCase(
+        emailValidatorStub,
+        encrypterHash,
+        userRepositoryStub,
+    );
+
+    return { sut, emailValidatorStub, encrypterHash, userRepositoryStub };
 }
 
 describe("CreateUserUseCase", () => {
@@ -188,4 +218,64 @@ describe("CreateUserUseCase", () => {
 
         expect(encrypterSpy).toHaveBeenCalledWith(fakeUser.password);
     });
+
+    it("Should call userRepository with correct values", async () => {
+        const { sut, userRepositoryStub } = makeSut();
+
+        const fakeUser = {
+            name: "test",
+            lastName: "da silva",
+            email: "test@gmail.com",
+            password: "123456",
+            passwordConfirmation: "123456",
+        };
+
+        const peito = {
+            name: "test",
+            lastName: "da silva",
+            email: "test@gmail.com",
+            password: "hashed_pas",
+        };
+
+        const userRepositorySpy = jest.spyOn(userRepositoryStub, "add");
+
+        await sut.execute(fakeUser);
+
+        expect(userRepositorySpy).toHaveBeenCalledWith(peito);
+    });
+
+    it("Should return a correct user if create was sucess", async () => {
+        const { sut } = makeSut();
+
+        const fakeUser = {
+            name: "test",
+            lastName: "da silva",
+            email: "test@gmail.com",
+            password: "123456",
+            passwordConfirmation: "123456",
+        };
+
+        const createdUser = await sut.execute(fakeUser);
+
+        expect(createdUser).toHaveProperty("id");
+    });
+
+    it("Should return a correct user findById", async () => {
+        const { sut, userRepositoryStub } = makeSut();
+
+        const fakeUser = {
+            name: "test",
+            lastName: "da silva",
+            email: "test@gmail.com",
+            password: "123456",
+            passwordConfirmation: "123456",
+        };
+
+        const createdUser = await sut.execute(fakeUser);
+        const findedUser = await userRepositoryStub.findById(createdUser.id);
+
+        expect(createdUser).toEqual(findedUser);
+    });
+
+    it("Should not possible create a user with email already exists", () => {});
 });
